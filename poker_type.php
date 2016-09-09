6fe5d3428c90f9de60151488f0f23838693c4a15
anedzyl <anedzyl@mobile01.com>
2016-09-09 12:02:58 +0800

    Two Pairs Three of a kind

--- poker_type.php
+++ poker_type.php
@@ -4,22 +4,47 @@
     $game_cards_set = array();
     $players = -1;
 
-    echo "Game Start! Please tell me how many players want to play this game[0~10]: ";
-    fscanf(STDIN, "%d\n", $players);
-    while (!is_int($players) || $players < 0 || $players > 10) {
-        echo "Please tell me a positive integer number[0~10]. How many players want to play this game: ";
-        fscanf(STDIN, "%d\n", $players);
-    }
-    if ($players == 0) {
-        echo "No one here. Have a nice day, Bye!\n";
-        exit(0);
-    }
+    
 
+    start_game_info($players);
     shuffle_cards($cards, $suffled_cards);
     deal($suffled_cards, $players, $game_cards_set);
     card_type($players, $game_cards_set);
 
 
+    //Four of a Kind & Full house & Three of a kind & Two Pairs & One Pairwill & High card will be found here
+    function pair_or_more(&$player_cards_set){
+        $test = array(3, 3, 4, 4, 5);
+        foreach ($test as $key => $value) {
+            $test[$key] = $value % 13;
+        }
+        $test_unique = array_unique($test);
+        //print_r($test);
+        //print_r($test_unique);
+        switch (count($test_unique)) {
+                case 2:
+
+                    break;
+                case 3:
+                    $type = max(array_count_values($test));
+                    echo "string" . $type;
+                    if ($type == 3) {
+                        array_push($test, 700); //Three of a kind
+                    }
+                    if ($type == 2) {
+                        array_push($test, 500); //Two Pairs
+                    }
+                    break;
+                case 4:
+                    array_push($test, 300); //One Pair
+                    break;
+                default:
+                    array_push($test, 100); //High card
+                    break;
+        }
+        print_r($test);
+
+    }
 
     // for flush
     function bool_flush($player_cards_set){
@@ -27,8 +52,7 @@
         for ($i = 0; $i < 4; $i++) {
             if ((int)($player_cards_set[$i + 1] / 13) == (int)($player_cards_set[$i] / 13)) {
             }
-            else
-            {
+            else{
                 $is_flush = 0;
                 return false;
             }
@@ -39,7 +63,7 @@
     }
 
     //Straight & Straight Flush will be found here
-    function Straight(&$player_cards_set){
+    function straight(&$player_cards_set){
         $is_straight = -1;
         //straight
         for ($i = 0; $i < 4; $i++) { 
@@ -48,7 +72,7 @@
             else
             {
                 $is_straight = 0;
-                break;
+                $i = 5;
             }
         }
         if ( $is_straight != 0) {
@@ -56,26 +80,23 @@
                 array_push($player_cards_set, 1700); //Straight Flush
             }
             else{
-                array_push($player_cards_set, 900);
+                array_push($player_cards_set, 900); //Flush
             }
-        }
-        else{
-            break;
         }
         print_r($player_cards_set);
     }
 
-
     function card_type($players, &$game_cards_set){
         for ($i = 0; $i < $players; $i++) { 
-            Straight($game_cards_set[$i]);
-            if (bool_flush($game_cards_set[$i]) {
+            straight($game_cards_set[$i]);
+            if (bool_flush($game_cards_set[$i])) {
                 array_push($player_cards_set, 1100);
             }
+            pair_or_more($game_cards_set[$i]);
+
         }
         //print_r($game_cards_set);
     }
-
 
     function deal(&$suffled_cards, $players, &$game_cards_set){
         //prepare game_cards_set space for players
@@ -161,6 +182,19 @@
             if ($i % 13 == 12) {
                 echo "\n";
             }
+        }
+    }
+
+    function start_game_info(&$players){
+        echo "Game Start! Please tell me how many players want to play this game[0~10]: ";
+        fscanf(STDIN, "%d\n", $players);
+        while (!is_int($players) || $players < 0 || $players > 10) {
+            echo "Please tell me a positive integer number[0~10]. How many players want to play this game: ";
+            fscanf(STDIN, "%d\n", $players);
+        }
+        if ($players == 0) {
+            echo "No one here. Have a nice day, Bye!\n";
+            exit(0);
         }
     }
 ?>
